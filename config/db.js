5b67a9ea21fa67a3da5db052993374ef49a7f28a
andres.mera <feli041089@gmail.com>
2023-09-22 14:31:36 -0500

    Agregando más información al log de conexión

--- config/db.js
+++ config/db.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 export const db = async () => {
   try {
-    const db = await mongoose.connect(process.env.MONGO_URI    );
-
-    console.log("Se conecto exitosamente");
+    const db = await mongoose.connect(process.env.MONGO_URI);
+    const url = `${db.connection.host}:${db.connection.port}`;
+    console.log(`MongoDB se conectó correctamente: ${url}`);
   } catch (error) {
     console.log(`Error :${error.message}`);
     process.exit(1);
