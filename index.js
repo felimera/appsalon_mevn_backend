03ec162c601d3b4fd33781dcc91faa4eebde9232
andres.mera <feli041089@gmail.com>
2023-09-22 09:57:30 -0500

    Respuestas JSON en Express

--- index.js
+++ index.js
@@ -4,7 +4,19 @@ const express = require("express");
 const app = express();
 // Definir una ruta
 app.get("/", (req, res) => {
-  res.send("Hola");
+  const products = [
+    {
+      id: 1,
+      price: 30,
+      name: "Laptop",
+    },
+    {
+      id: 2,
+      price: 50,
+      name: "Monitor",
+    },
+  ];
+  res.json(products);
 });
 // Definir un puerto
 const PORT = process.env.PORT || 4000;
