7cdd0b7e0f7f9b84b80ad9ef3f00a3b1453f01bd
andres.mera <feli041089@gmail.com>
2023-09-25 08:42:04 -0500

    Leer los datos enviados hacia la API

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import express from "express"; // ESM
-import dotenv from 'dotenv';
-import colors from 'colors';
+import dotenv from "dotenv";
+import colors from "colors";
 import { db } from "./config/db.js";
 import servicesRoutes from "./routes/servicesRoutes.js";
 
 // Variables de entorno
-dotenv.config()
+dotenv.config();
 // Configurar la app
 const app = express();
+// Leer datos via body
+app.use(express.json());
 // Conectando a BD
 db();
 // Definir una ruta
@@ -16,5 +18,8 @@ app.use("/api/services", servicesRoutes);
 const PORT = process.env.PORT || 4000;
 // Arracar la app
 app.listen(PORT, () => {
-  console.log(colors.blue("El servidor se esta ejecutando en el puerto : "), colors.blue.bold(PORT));
+  console.log(
+    colors.blue("El servidor se esta ejecutando en el puerto : "),
+    colors.blue.bold(PORT)
+  );
 });
