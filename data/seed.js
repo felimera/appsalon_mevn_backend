f9ddfd6ba186168effbd7a23920185c8978341ce
andres.mera <feli041089@gmail.com>
2023-09-25 14:48:21 -0500

    Insertando Registros de forma Másiva con insertMany.

--- data/seed.js
+++ data/seed.js
@@ -1,17 +1,29 @@
+import dotenv from "dotenv";
+import colors from "colors";
+import { db } from "../config/db.js";
+import Services from "../models/Services.js";
+import { services } from "./beautyServices.js";
 
+dotenv.config();
+await db();
 
-function seedDB(){
-    console.log('desde seedDB');
+async function seedDB() {
+  try {
+    await Services.insertMany(services);
+    console.log(colors.green.bold("Se agregaron los datos correctamente."));
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
 }
 
-function clearDB(){
-    console.log('desde clearDB');
+function clearDB() {
+  console.log("desde clearDB");
 }
 
-if(process.argv[2]==='--import')
-{
-seedDB();
-}
-else{
-    clearDB();
+if (process.argv[2] === "--import") {
+  seedDB();
+} else {
+  clearDB();
 }
