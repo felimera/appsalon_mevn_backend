c2e5dcc6b80f74d71f9bd78d5a469e0fc2255053
andres.mera <feli041089@gmail.com>
2023-09-29 09:44:06 -0500

    Ajustando la URL para que la confirmación sea desde Vue.js

--- emails/authEmailService.js
+++ emails/authEmailService.js
@@ -2,21 +2,21 @@ import { createTransport } from "../config/nodemailer.js";
 
 export async function sendEmailVerification({ name, email, token }) {
   const transporter = createTransport(
-    "sandbox.smtp.mailtrap.io",
-    2525,
-    "d05bbbbd01027b",
-    "10f366483fcbc6"
+    process.env.EMAIL_HOST,
+    process.env.EMAIL_PORT,
+    process.env.EMAIL_USER,
+    process.env.EMAIL_PASS
   );
 
   // Enviar el email
   const info = await transporter.sendMail({
-    from: "AppSalon",
+    from: "AppSalon <cuentas@appsalon.com>",
     to: email,
     subject: "AppSalon - Confirma tu cuenta",
     text: "AppSalon - Confirma tu cuenta",
     html: `<p>Hola: ${name}, confirma tu cuenta en AppSalon</p>
         <p>Tu cuenta esta casi lista, solo debes confirmar en el siguiente enlace:</p>
-        <a href="http://localhost:4000/api/auth/verify/${token}">Confirmar cuenta</a>
+        <a href="${process.env.FRONTEND_URL}/auth/confirmar-cuenta/${token}">Confirmar cuenta</a>
         <p>Si tu no creaste esta cuenta, puedes ignorar este mensaje.</p> `,
   });
 
