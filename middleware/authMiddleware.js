960af6d5091c05efade542add4deb1748565b45f
andres.mera <feli041089@gmail.com>
2023-10-02 09:59:44 -0500

    ¿Como enviar el JWT jacia el servidor?

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,6 +1,12 @@
 
-const authMiddleware=()=>{
-    console.log('desde authMiddleware')
+const authMiddleware=(req,res,next)=>{
+    if(req.headers.authorization&&req.headers.authorization.startsWith('Bearer'))
+    {
+
+    }else{
+        const error=new Error('Token no válido o inexistente');
+        res.status(403).json({msg:error.message});
+    }
 }
 
 export default authMiddleware;
