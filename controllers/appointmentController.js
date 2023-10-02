3c0df17698fe04ec97f468c43c7a42ac6179a325
andres.mera <feli041089@gmail.com>
2023-10-02 14:30:47 -0500

    Formateando fecha de String a ISO para MongoDB

--- controllers/appointmentController.js
+++ controllers/appointmentController.js
@@ -1,10 +1,12 @@
 import Appointment from '../models/Appointment.js';
 
 const createAppointment = async (req, res) => {
+    const appointment = req.body;
+    appointment.user = req.user._id.toString();
     try {
-        const appointment=req.body;
-        appointment.user=req.user._id.toString();
-        console.log('appointment',appointment)
+        const newAppointment = new Appointment(appointment);
+        await newAppointment.save();
+        res.json({ msg: 'Cita Almacenada Correctamente' });
     } catch (error) {
         console.log(error)
     }
