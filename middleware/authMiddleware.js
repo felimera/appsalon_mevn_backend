794aa438f9fd455f3e8dab9419b259a57ebff2c9
andres.mera <feli041089@gmail.com>
2023-10-02 10:07:27 -0500

    ¿Como Verificar un JWT en el Servidor?

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,8 +1,16 @@
+import jwt from 'jsonwebtoken';
 
 const authMiddleware=(req,res,next)=>{
     if(req.headers.authorization&&req.headers.authorization.startsWith('Bearer'))
     {
-
+try {
+    const token=req.headers.authorization.split(' ')[1];
+    const decode=jwt.verify(token,process.env.JWT_SECRET);
+    console.log(decode);
+} catch {
+    const error=new Error('Token no válido');
+        res.status(403).json({msg:error.message});
+}
     }else{
         const error=new Error('Token no válido o inexistente');
         res.status(403).json({msg:error.message});
