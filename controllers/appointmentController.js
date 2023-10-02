4c630c3b8fd2545d2b2a6851784beb133db9b5a9
andres.mera <feli041089@gmail.com>
2023-10-02 14:44:50 -0500

    Mostrando una alerta cuando una cita se realiza correctamente.

--- controllers/appointmentController.js
+++ controllers/appointmentController.js
@@ -6,7 +6,7 @@ const createAppointment = async (req, res) => {
     try {
         const newAppointment = new Appointment(appointment);
         await newAppointment.save();
-        res.json({ msg: 'Cita Almacenada Correctamente' });
+        res.json({ msg: 'Tu Reservación se realizó Correctamente' });
     } catch (error) {
         console.log(error)
     }
