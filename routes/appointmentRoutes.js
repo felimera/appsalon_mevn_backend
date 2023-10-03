2f17148e396942ff28ad70bad39c2f5fbbb48206
andres.mera <feli041089@gmail.com>
2023-10-03 08:52:44 -0500

    Obtener citas por fechas Part 02

--- routes/appointmentRoutes.js
+++ routes/appointmentRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createAppointment,getAppointmentsByDate } from '../controllers/appointmentController.js';
+import { createAppointment, getAppointmentsByDate } from '../controllers/appointmentController.js';
 import authMiddleware from '../middleware/authMiddleware.js';
 
 const router = express.Router();
